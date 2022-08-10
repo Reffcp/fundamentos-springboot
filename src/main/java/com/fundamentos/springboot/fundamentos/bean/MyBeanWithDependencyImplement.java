385823b66e0cfe40460c0698043a85b60bfcb6ea
reffcp <yo@cristiancovarrubias.online>
2022-08-10 08:41:24 -0500

    Ejemplo de dependencia propia

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -0,0 +1,16 @@
+package com.fundamentos.springboot.fundamentos.bean;
+
+public class MyBeanWithDependencyImplement implements MyBeanWithDependency{
+
+    private MyOperation myOperation;
+
+    public MyBeanWithDependencyImplement(MyOperation myOperation) {
+        this.myOperation = myOperation;
+    }
+
+    @Override
+    public void printWithDependency() {
+        System.out.println("resultado suma: " + myOperation.sum(10));
+        System.out.println("Hoal desde un bean con dependecia");
+    }
+}
